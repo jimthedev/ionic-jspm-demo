ba84488cc0e8a43a2a6404f375c549e1c8f9b4ee
Jim Cummins <jcummins@fisdap.net>
2015-02-13 22:24:58 -0600

    Add medium button on contact page

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ System.config({
   "paths": {
     "*": "*.js",
     "ionic-jspm-demo/*": "lib/*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
@@ -12,10 +13,11 @@ System.config({
     "angular-animate": "github:angular/bower-angular-animate@1.3.12",
     "angular-sanitize": "github:angular/bower-angular-sanitize@1.3.12",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
+    "css": "github:systemjs/plugin-css@0.1.0",
+    "font-awesome": "npm:font-awesome@4.3.0",
     "ionic": "github:driftyco/ionic-bower@1.0.0-beta.14",
     "ionicons": "github:driftyco/ionicons@2.0.1",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "css": "github:systemjs/plugin-css@0.1.0",
     "github:angular-ui/ui-router@0.2.13": {
       "angular": "github:angular/bower-angular@1.3.12"
     },
@@ -30,8 +32,7 @@ System.config({
       "angular-animate": "github:angular/bower-angular-animate@1.3.12",
       "angular-sanitize": "github:angular/bower-angular-sanitize@1.3.12",
       "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
-      "css": "github:systemjs/plugin-css@0.1.0",
-      "ionicons": "github:driftyco/ionicicons@2.0.1"
+      "css": "github:systemjs/plugin-css@0.1.0"
     },
     "github:driftyco/ionicons@2.0.1": {
       "css": "github:systemjs/plugin-css@0.1.0"
